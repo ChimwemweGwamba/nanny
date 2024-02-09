4e7bf9b35100ba98223660885efc8f288f33b1d6
ChimwemweGwamba <chimwemwebengist@gmail.com>
2024-02-09 12:21:21 +0200

    added sign up page

--- src/components/becomeNanny.js
+++ src/components/becomeNanny.js
@@ -1,8 +1,37 @@
-import React from 'react'
+import React from "react"
+import bannerImg from "../img/banner.png"
 
 function BecomeNanny() {
   return (
-    <div>BecomeNanny</div>
+    <div className="w-11/12 mx-auto py-20 lg:py-32 border">
+      <div className="flex lg:flex-row flex-col justify-between items-center gap-20 border">
+        <div className="lg:w-6/12 py-20 lg:py-0">
+          <p className="text-3xl lg:text-5xl text-secondary font-semibold leading-tight lg:leading-tight mb-10">
+            Looking for work? Become a certified Nanny Today
+          </p>
+
+          <p className="mb-10 text-sm">
+            Lorem ipsum dolor sit amet consectetur. Feugiat ut aliquet sit pellentesque sollicitudin. Egestas faucibus
+            lacus diam in senectus consectetur. Mattis elit adipiscing quisque tellus scelerisque vehicula ante nunc.
+            Tellus consequat nisl quis nisl justo.
+          </p>
+
+          <div className="">
+            <button className="text-white text-sm p-4 bg-secondary mr-10 rounded-3xl">
+              Sign Up
+            </button>
+
+            <button className="text-white text-sm p-4 bg-secondary mr-10 rounded-3xl">
+              Training Programs
+            </button>
+          </div>
+        </div>
+
+        <div className="lg:w-5/12 border bg-red-500">
+          <img src={bannerImg} alt="image" className="w-full h-auto object-cover" />
+        </div>
+      </div>
+    </div>
   )
 }
 
