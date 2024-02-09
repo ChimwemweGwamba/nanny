2da1ea55f89ed20419b06fb01480c4d84519d303
ChimwemweGwamba <chimwemwebengist@gmail.com>
2024-02-09 10:44:27 +0200

    added why us component

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,10 +1,12 @@
 import React from "react"
 import Banner from "../components/banner"
+import WhyUs from "../components/whyUs"
 
 function Home() {
   return (
     <div className="lg:w-[98%] w-[95%] mx-auto">
-      <Banner />
+      {/* <Banner /> */}
+      <WhyUs />
     </div>
   )
 }
